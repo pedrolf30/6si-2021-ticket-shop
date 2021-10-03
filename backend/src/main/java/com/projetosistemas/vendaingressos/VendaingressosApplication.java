f2b746ac177747bb37f35b01f92fbe0a92a74629
pedrolf30 <pedrolf30@gmail.com>
2021-10-02 23:56:13 -0300

    Criando projeto spring boot

---
+++ backend/src/main/java/com/projetosistemas/vendaingressos/VendaingressosApplication.java
@@ -0,0 +1,13 @@
+package com.projetosistemas.vendaingressos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaingressosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaingressosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projetosistemas/vendaingressos/VendaingressosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetosistemas.vendaingressos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendaingressosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
